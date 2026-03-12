3d28cd0b5c830f78920f4b3063ebf014df648567
Vladimir Goemaere <vladgoemaere@MacBook-Air-de-Vladimir-2.local>
2026-03-12 10:49:21 +0100

    add all

--- src/decoder.py
+++ src/decoder.py
@@ -6,14 +6,15 @@ from llm_sdk.llm_sdk import Small_LLM_Model
 class JSONDecoder:
     def __init__(self, model: Small_LLM_Model, functions: List[Any]):
         self.model = model
-        self.functions = {f['name']: f for f in functions}
+        self.functions = {f.name: f for f in functions}
         vocab_path = self.model.get_path_to_vocab_file
         with open(vocab_path, 'r', encoding='utf-8') as f:
             tokenizer_data: Dict[str, str] = json.load(f)
             self.vocab: Dict[str, str] = tokenizer_data.get("model",
                                                             {}).get("vocab",
                                                                     {})
-            self.id_to_token = {v: k for k, v in self.vocab.items()}
+            self.id_to_token = {int(v): k for k, v in self.vocab.items()}
+            self.vocab_size = len(self.vocab)
 
     def _is_valid_prefix(self, prefix: str) -> bool:
         if not prefix:
